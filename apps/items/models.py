75bb0d22cd02530da18264a4ab030db6f0a2cf81
SazidAhmed <sazidahmed.official@gmail.com>
2021-03-25 21:10:47 +0600

    item model added

--- apps/items/models.py
+++ apps/items/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=255)
+    ordering = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ('ordering',)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return '/%s/' % (self.slug)
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(
+        Category, related_name="subcategory", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name_plural = 'Subcategories'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.CASCADE
+    )
+    subcategory = models.ForeignKey(
+        SubCategory, related_name="products", on_delete=models.CASCADE
+    )
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
---
+++ apps/items/schema.py
@@ -0,0 +1,36 @@
+import graphene
+from graphene import relay, ObjectType
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+
+from .models import Category, Ingredient
+
+
+# Graphene will automatically map the Category model's fields onto the CategoryNode.
+# This is configured in the CategoryNode's Meta class (as you can see below)
+class CategoryNode(DjangoObjectType):
+    class Meta:
+        model = Category
+        filter_fields = ['name', 'ingredients']
+        interfaces = (relay.Node, )
+
+
+class IngredientNode(DjangoObjectType):
+    class Meta:
+        model = Ingredient
+        # Allow for some more advanced filtering here
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'notes': ['exact', 'icontains'],
+            'category': ['exact'],
+            'category__name': ['exact'],
+        }
+        interfaces = (relay.Node, )
+
+
+class Query(graphene.ObjectType):
+    category = relay.Node.Field(CategoryNode)
+    all_categories = DjangoFilterConnectionField(CategoryNode)
+
+    ingredient = relay.Node.Field(IngredientNode)
+    all_ingredients = DjangoFilterConnectionField(IngredientNode)
